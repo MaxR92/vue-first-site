86b1bc07a6e1109faeadce9fbac87ecbbe68404b
Max R <Max.Roese@googlemail.com>
2023-03-03 15:13:21 +0100

    project finished

---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const showButton = ref(true);
+const greeting = "Welcome to this site";
+const message = ref("This site contains information about available jobs.");
+const changeMessage = () => {
+  message.value = "This site also contains skills you need for IT jobs.";
+  showButton.value = false;
+};
+</script>
+
+<template>
+  <div class="welcome">
+    <h2>{{ greeting }}</h2>
+    <p>{{ message }}</p>
+    <button v-if="showButton" @click="changeMessage">more info</button>
+  </div>
+</template>
+
+<style scoped>
+h2 {
+  color: orange;
+}
+p {
+  color: #aaa;
+  font-style: italic;
+}
+</style>
